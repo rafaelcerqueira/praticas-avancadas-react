c2836f4e2533e2125e1418ac6c3b61865f91eab6
Rafael Freitas <rafaelcerqueira92@gmail.com>
2022-01-25 21:57:23 -0300

    Ciclo de vida e suas fases

---
+++ advanced-reactjs/src/Twitter.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Twitter extends Component {
+
+    componentDidMount () {
+        const { posts } = this.props;
+        console.log('componentDidMount', posts);
+    }
+
+    componentWillUnmount () {
+        console.log('componentWillUnmount: fui removido.');
+    }
+  render () {
+
+    const posts = [
+      {
+        title: 'xpto',
+        description: 'foo'
+      },
+      {
+        title: 'xpto',
+        description: 'foo'
+      }
+    ]
+    return (
+      <div>
+        teste
+      </div>
+    )
+  }
+}
+
+export default Twitter
